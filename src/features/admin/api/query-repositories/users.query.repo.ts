36af149ff988c2f3404bd50024c8d6231f2b18e2
iKlimkin <ghbrot@gmail.com>
2024-07-21 12:59:53 +0300

    getAllUsers with ban logic

--- src/features/admin/api/query-repositories/users.query.repo.ts
+++ src/features/admin/api/query-repositories/users.query.repo.ts
@@ -1,16 +1,28 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { PaginationViewModel } from '../../../../domain/sorting-base-filter';
 import { getPagination } from '../../../../infra/utils/get-pagination';
 import { UserAccount } from '../../domain/entities/user-account.entity';
-import { SAQueryFilter } from '../models/outputSA.models.ts/sa-query.filter';
-import { getSAViewModel } from '../models/userAdmin.view.models/saView.model';
-import { SAViewType } from '../models/userAdmin.view.models/userAdmin.view.model';
+import { BloggerBannedUsersQueryFilter } from '../models/outputSA.models.ts/blogger-banned-users.query';
+import {
+  BanStatus,
+  SAQueryFilter,
+} from '../models/outputSA.models.ts/sa-query.filter';
+import {
+  getBloggerBannedUsersView,
+  getSAViewModel,
+} from '../models/user.view.models/saView.model';
+import {
+  BloggerBannedUsersViewType,
+  SAViewType,
+  SAViewWithBannedUsersType,
+} from '../models/user.view.models/userAdmin.view-type';
 
 @Injectable()
 export class UsersQueryRepo {
   constructor(
+    @InjectDataSource() private dataSource: DataSource,
     @InjectRepository(UserAccount)
     private readonly userAccounts: Repository<UserAccount>,
   ) {}
@@ -18,33 +30,109 @@ export class UsersQueryRepo {
   async getAllUsers(
     queryOptions: SAQueryFilter,
   ): Promise<PaginationViewModel<SAViewType>> {
-    const { searchEmailTerm, searchLoginTerm } = queryOptions;
+    const { searchEmailTerm, searchLoginTerm, banStatus } = queryOptions;
 
     const { pageNumber, pageSize, skip, sortBy, sortDirection } =
       getPagination(queryOptions);
 
-    const searchTerms = [
-      `%${searchLoginTerm ? searchLoginTerm : ''}%`,
-      `%${searchEmailTerm ? searchEmailTerm : ''}%`,
+    const [login, email] = [
+      `%${searchLoginTerm || ''}%`,
+      `%${searchEmailTerm || ''}%`,
     ];
 
-    const [users, usersCount] = await this.userAccounts
-      .createQueryBuilder('user')
-      .where('user.login ILIKE :login OR user.email ILIKE :email', {
-        login: searchTerms[0],
-        email: searchTerms[1],
-      })
-      .orderBy(
-        sortBy !== 'created_at'
-          ? `user.${sortBy} COLLATE "C"`
-          : 'user.created_at',
-        sortDirection,
-      )
-      .skip(skip)
-      .take(pageSize)
-      .getManyAndCount();
+    // raw users
+    // let totalCountQuery = `
+    //   SELECT COUNT(*) AS "totalCount"
+    //   FROM user_account u
+    //   LEFT JOIN user_bans bans ON u.id = bans."userId"
+    //   WHERE (u.login ILIKE $1 OR u.email ILIKE $2)
+    // `;
 
-    return new PaginationViewModel<SAViewType>(
+    // let addWhereBanStatus = '';
+    // if (banStatus === BanStatus.banned) {
+    //   addWhereBanStatus += 'AND bans."isBanned" = true';
+    // } else if (banStatus === BanStatus.notBanned) {
+    //   addWhereBanStatus += 'AND (bans IS NULL OR bans."isBanned" = false)';
+    // }
+    // totalCountQuery = totalCountQuery.concat(addWhereBanStatus);
+
+    // const [{ totalCount }] = await this.dataSource.query(totalCountQuery, [
+    //   login,
+    //   email,
+    // ]);
+
+    // let getUsersQuery = `
+    //   SELECT
+    //   u.login,
+    //   u.email,
+    //   u.id,
+    //   u.created_at "createdAt",
+    //   bans."banReason",
+    //   bans."isBanned",
+    //   bans."banDate"
+    //   FROM user_account u
+    //   LEFT JOIN user_bans bans ON u.id = bans."userId"
+    //   WHERE (u.login ILIKE $1 OR u.email ILIKE $2)
+    // `;
+    // getUsersQuery = getUsersQuery.concat(addWhereBanStatus);
+
+    // getUsersQuery += `
+    //   ORDER BY u.${
+    //     sortBy === 'created_at' ? 'created_at' : sortBy + ' COLLATE "C"'
+    //   } ${sortDirection}
+    //   LIMIT $3
+    //   OFFSET $4;`;
+
+    // const rawUsers = await this.dataSource.query(getUsersQuery, [
+    //   login,
+    //   email,
+    //   pageSize,
+    //   skip,
+    // ]);
+
+    // return new PaginationViewModel<SAViewWithBannedUsersType>(
+    //   rawUsers.map((rawUser) => ({
+    //     id: rawUser.id,
+    //     login: rawUser.login,
+    //     email: rawUser.email,
+    //     createdAt: rawUser.createdAt.toISOString(),
+    //     banInfo: {
+    //       isBanned: rawUser.isBanned || false,
+    //       banDate: rawUser.banDate?.toISOString() || null,
+    //       banReason: rawUser.banReason || null,
+    //     },
+    //   })),
+    //   pageNumber,
+    //   pageSize,
+    //   totalCount,
+    // );
+
+    const queryBuilder = this.userAccounts
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.userBan', 'ban')
+      .where('(user.login ILIKE :login', { login })
+      .orWhere('user.email ILIKE :email)', { email });
+    console.log({banStatus});
+    
+    if (banStatus === BanStatus.banned) {
+      queryBuilder.andWhere('(ban.isBanned = true)');
+    } else if (banStatus === BanStatus.notBanned) {
+      queryBuilder.andWhere('(ban IS NULL OR ban.isBanned = false)');
+    }
+
+    queryBuilder
+      .orderBy('user.' + sortBy, sortDirection)
+      // .orderBy(
+      //   sortBy !== 'created_at' ? `user.${sortBy}` : `user.created_at`,
+      //   sortDirection,
+      // )
+      .skip(skip)
+      .take(pageSize);
+    console.log(queryBuilder.getQueryAndParameters());
+    
+    const [users, usersCount] = await queryBuilder.getManyAndCount();
+
+    return new PaginationViewModel<SAViewWithBannedUsersType>(
       users.map(getSAViewModel),
       pageNumber,
       pageSize,
@@ -58,7 +146,49 @@ export class UsersQueryRepo {
     );
   }
 
-  async getUserById(userId: string): Promise<SAViewType | null> {
+  async getBannedUsersForBlog(
+    blogId: string,
+    queryOptions: BloggerBannedUsersQueryFilter,
+  ): Promise<PaginationViewModel<BloggerBannedUsersViewType>> {
+    const { searchLoginTerm } = queryOptions;
+    const { pageNumber, pageSize, skip, sortBy, sortDirection } =
+      getPagination(queryOptions);
+
+    const [login] = [`%${searchLoginTerm || ''}%`];
+
+    const queryBuilder = this.userAccounts.createQueryBuilder('user');
+
+    queryBuilder
+      .select('user.login', 'login')
+      .addSelect('user.id', 'id')
+      .where('user.login ILIKE :login', { login })
+      .leftJoin('user.bloggerBans', 'bb')
+      .addSelect('bb.banDate', 'banDate')
+      .addSelect('bb.banReason', 'banReason')
+      .andWhere(
+        'user.id IN (SELECT "userId" FROM user_blogger_bans WHERE "isBanned" = true AND "blogId" = :blogId)',
+        { blogId },
+      )
+      .orderBy('user.' + sortBy, sortDirection)
+      .skip(skip)
+      .take(pageSize);
+
+    try {
+      const users = await queryBuilder.getRawMany();
+      const usersCount = await queryBuilder.getCount();
+
+      return new PaginationViewModel<BloggerBannedUsersViewType>(
+        users.map(getBloggerBannedUsersView),
+        pageNumber,
+        pageSize,
+        usersCount,
+      );
+    } catch (error) {
+      console.error(`get blogger banned users: ${error}`);
+    }
+  }
+
+  async getById(userId: string): Promise<SAViewType | null> {
     try {
       const result = await this.userAccounts.findOneBy({ id: userId });
 
